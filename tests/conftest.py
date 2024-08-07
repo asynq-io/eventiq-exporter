5aff3a83a4275f8aead8ab701311046923b40f82
RaRhAeu <rarha_eu@protonmail.com>
2024-08-07 23:46:03 +0200

    feat: update eventiq

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import asyncio
-from datetime import date
+from contextlib import suppress
 from unittest.mock import AsyncMock
 
 import pytest
 import pytest_asyncio
 from eventiq import CloudEvent, Service
 from eventiq.backends.stub import StubBroker
+from eventiq.utils import utc_now
 
 
 @pytest.fixture(scope="session")
@@ -13,12 +14,12 @@ def event_loop():
     return asyncio.get_event_loop()
 
 
-@pytest.fixture
+@pytest.fixture()
 def broker():
     return StubBroker()
 
 
-@pytest.fixture
+@pytest.fixture()
 def service(broker):
     return Service(broker=broker, name="test_service")
 
@@ -32,7 +33,7 @@ def handler():
     return example_handler
 
 
-@pytest.fixture
+@pytest.fixture()
 def mock_consumer(handler):
     mock = AsyncMock(spec=handler)
     mock.__annotations__ = handler.__annotations__
@@ -42,15 +43,18 @@ def mock_consumer(handler):
 @pytest_asyncio.fixture()
 async def running_service(service: Service, mock_consumer):
     service.subscribe(topic="test_topic")(mock_consumer)
-
-    async with service.context():
-        yield service
+    task = asyncio.create_task(service.run(enable_signal_handler=False))
+    await asyncio.sleep(0)
+    yield service
+    with suppress(asyncio.CancelledError):
+        task.cancel()
+        await task
 
 
 @pytest.fixture()
 def ce() -> CloudEvent:
     return CloudEvent.new(
-        {"today": date.today().isoformat(), "arr": [1, "2", 3.0]},
+        {"today": utc_now().date(), "arr": [1, "2", 3.0]},
         type="TestEvent",
         topic="test_topic",
     )
